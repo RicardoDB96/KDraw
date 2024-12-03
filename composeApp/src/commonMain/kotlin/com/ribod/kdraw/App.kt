d59f2286cb2a9088dc2217173818fdbd69a15fa6
Ricardo DB <ricardo.domber96@gmail.com>
2024-12-02 23:30:04 -0600

    chore: add KoinContext to app

--- composeApp/src/commonMain/kotlin/com/ribod/kdraw/App.kt
+++ composeApp/src/commonMain/kotlin/com/ribod/kdraw/App.kt
@@ -4,11 +4,14 @@ import androidx.compose.runtime.Composable
 import com.ribod.kdraw.ui.core.navigation.NavigationWrapper
 import com.ribod.kdraw.ui.core.theme.KDrawTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.KoinContext
 
 @Composable
 @Preview
 fun App() {
-    KDrawTheme {
-        NavigationWrapper()
+    KoinContext {
+        KDrawTheme {
+            NavigationWrapper()
+        }
     }
 }
