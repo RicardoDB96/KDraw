91e3ac608dd758b0fb8afe49337e5c130bbc8895
Ricardo DB <ricardo.domber96@gmail.com>
2025-01-13 19:27:37 -0600

    remove unused files

--- composeApp/src/commonMain/kotlin/com/ribod/kdraw/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.ribod.kdraw
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/com/ribod/kdraw/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.ribod.kdraw
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
